2f6e715e4a7403d5caf0ca24a615991b8dd8a9eb
kalpalathika <kalpalathika1@gmail.com>
2025-02-08 23:17:17 -0500

    refactored routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-  // Link,
-  // BrowserRouter
 } from 'react-router-dom';
-// import App from './App.tsx'
 import LoginPage from './pages/LoginPage.tsx';
 import HomePage from './pages/HomePage.tsx';
 import FavouritesPage from './pages/FavouritesPage.tsx';
@@ -18,37 +15,42 @@ import { RecoilRoot } from 'recoil';
 
 const queryClient = new QueryClient();
 
-const PrivateRoute = ({children} : {children: JSX.Element}) => {
-      const isAuthenticated = !!localStorage.getItem("token");
+const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+  const isAuthenticated = !!localStorage.getItem('token');
+  return isAuthenticated ? children : <Navigate to="/" replace />;
+};
 
-      return isAuthenticated ? children : <Navigate to="/" replace/>
-
-}
+// Layout component to wrap all routes
+const RouteLayout = () => (
+  <Routes>
+    <Route path="/" element={<LoginPage />} />
+    <Route
+      path="/home"
+      element={
+        <PrivateRoute>
+          <HomePage />
+        </PrivateRoute>
+      }
+    />
+    <Route
+      path="/favourites"
+      element={
+        <PrivateRoute>
+          <FavouritesPage />
+        </PrivateRoute>
+      }
+    />
+  </Routes>
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <Router>
-          <Routes>
-            <Route path="/" element={<LoginPage/>} />
-            <Route path="/home" 
-            element={
-              <PrivateRoute> 
-                <HomePage/>
-              </PrivateRoute>
-            } 
-            />
-            <Route path="/favourites" 
-            element={
-              <PrivateRoute>
-                <FavouritesPage/>
-              </PrivateRoute>
-              } 
-              />
-          </Routes>
+          <RouteLayout /> 
         </Router>
       </QueryClientProvider>
     </RecoilRoot>
   </StrictMode>
-)
+);
