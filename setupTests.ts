7c4339b18893b7fd588332d2f076e4e9410da487
kalpalathika <kalpalathika1@gmail.com>
2025-02-10 20:28:50 -0500

    added config as per testig env

--- setupTests.ts
+++
@@ -1,7 +0,0 @@
-// src/setupTests.ts
-import '@testing-library/jest-dom';
-
-import { TextEncoder, TextDecoder } from 'node:util';
-
-global.TextEncoder = TextEncoder as typeof global.TextEncoder;
-global.TextDecoder = TextDecoder as typeof global.TextDecoder;
