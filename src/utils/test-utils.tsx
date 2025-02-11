34dcddee4931087cb1abb75ef363b053aa32d7c9
kalpalathika <kalpalathika1@gmail.com>
2025-02-10 21:07:52 -0500

    Experimental- test config as per vite and netlify

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,9 +1,9 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from '../pages/LoginPage';
-import HomePage from '../pages/HomePage';
-import FavouritesPage from '../pages/FavouritesPage';
+import LoginPage from '../LoginPage';
+import HomePage from '../HomePage';
+import FavouritesPage from '../FavouritesPage';
 
 const queryClient = new QueryClient();
 
--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
+    setupFiles: ['./setupTests.ts'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
